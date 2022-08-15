c07c27922853369880b7e0a8c9a21b602486587e
Nowaaru <zackyboy135@gmail.com>
2022-08-15 00:22:03 -0700

    ...nothing?

--- src-tauri/src/handlers/mod.rs
+++ src-tauri/src/handlers/mod.rs
@@ -2,17 +2,17 @@ use tauri::Manager;
 
 #[tauri::command]
 pub fn splash_close(window: tauri::Window) -> Result<(), crate::errors::InternalError> {
-    if let Some(splashscreen) = window.get_window("splashscreen") {
-        match splashscreen.close() {
-            Ok(()) => return Ok(()),
-            Err(why) => {
-                return Err(crate::errors::InternalError::new(&*format!(
-                    "Splashscreen failed to close: {}",
-                    why
-                )))
-            }
-        }
-    } else {
-        Err(crate::errors::InternalError::new("No splash-screen found."))
-    }
+	if let Some(splashscreen) = window.get_window("splashscreen") {
+		match splashscreen.close() {
+			Ok(()) => return Ok(()),
+			Err(why) => {
+				return Err(crate::errors::InternalError::new(&*format!(
+					"Splashscreen failed to close: {}",
+					why
+				)))
+			}
+		}
+	} else {
+		Err(crate::errors::InternalError::new("No splash-screen found."))
+	}
 }
