5f7d34d813438d6284f9eabc64ab0594e2f46217
Nowaaru <zackyboy135@gmail.com>
2022-08-16 11:50:36 -0700

    [meta] Fix Vite's HMR being slow and unstable

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { swcReactRefresh } from "vite-plugin-swc-react-refresh";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        watch: {
-            usePolling: true,
-        },
-    },
+    plugins: [/*react()*/ tsconfigPaths(), swcReactRefresh()],
 });
