23bcd28f529d58b5ab92bd33da0528ecbf42dc11
Nowaaru <zackyboy135@gmail.com>
2022-08-15 22:22:23 -0700

    add chakra

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Page Imports
-import BaseClass from "./pages/main";
-import SplashScreen from "./pages/splash";
+import BaseClass from "pages/main";
+import Library from "pages/library";
+import SplashScreen from "pages/splash";
 
+// Utiltiy
+import DefaultThemeDark from "assets/themes/dark";
 // TODO: Instead of directly writing the routes with the target page
 //      we should use a function to setup any additional logic (i.e. redirecting),
 //      and then return the logic after all is done.
@@ -17,11 +21,15 @@ import SplashScreen from "./pages/splash";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<BaseClass />} />
-                <Route path="splash" element={<SplashScreen />} />
-            </Routes>
-        </BrowserRouter>
+        <React.StrictMode>
+            <ChakraProvider theme={extendTheme(DefaultThemeDark)}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Library />} />
+                        <Route path="splash" element={<SplashScreen />} />
+                    </Routes>
+                </BrowserRouter>
+            </ChakraProvider>
+        </React.StrictMode>
     );
 }
