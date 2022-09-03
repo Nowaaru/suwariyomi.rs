57e3d37bdfd3a59471e670a97edb7f39c0d5ed4a
Nowaaru <zackyboy135@gmail.com>
2022-09-03 02:27:50 -0700

    [frontend] Scrap canvas idea for now, rename mp_test.tsx to reader.tsx

--- src/components/mangapage.tsx
+++ src/components/mangapage.tsx
@@ -1,3 +1,66 @@
-export default function MangaPage() {
-    return <canvas />;
-}
+import { CanvasHTMLAttributes, useEffect, useRef, useState } from "react";
+import { StyleSheet, css } from "aphrodite";
+import useForceUpdate from "hooks/forceupdate";
+import _ from "lodash";
+
+type float = number;
+type MangaBlob = {
+    blob: Blob;
+    url?: never;
+};
+type MangaUrl = {
+    blob?: never;
+    url: string;
+};
+
+type MangaRoundedTrue = {
+    rounded: true;
+    radius: number;
+};
+
+type MangaRoundedFalse = {
+    rounded?: false;
+    radius?: never;
+};
+
+type MangaAppearance = {
+    baseZoom?: float; // TBA when I can be fucked to implement canvas-based manga pages because zooming and panning is a demonic thing to implement.
+} & (MangaRoundedTrue | MangaRoundedFalse);
+
+type MangaPageProps = (MangaBlob | MangaUrl) &
+    MangaAppearance & {
+        fit?: "width" | "height" | "comfortable";
+    };
+
+const MangaPage = (props: MangaPageProps) => {
+    const url = props.url || URL.createObjectURL(props.blob ?? new Blob());
+    const isComfortable = props.fit === "comfortable";
+    const padding = isComfortable ? "5%" : "0";
+    const styles = StyleSheet.create({
+        page: {
+            borderRadius: props.rounded ? props.radius : "0px",
+            [isComfortable ? "maxHeight" : `max${_.capitalize(props.fit)}`]:
+                "100%",
+        },
+    });
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "row",
+                verticalAlign: "middle",
+                alignItems: "center",
+                backgroundColor: "#0D1620",
+                width: "100vw",
+                height: "100vh",
+                padding,
+            }}
+        >
+            <img src={url} className={css(styles.page)} />
+        </div>
+    );
+};
+
+export default MangaPage;
---
+++ src/pages/reader.tsx
@@ -0,0 +1,23 @@
+import MangaPage from "components/mangapage";
+import Lightbar from "components/lightbar";
+import { StyleSheet, css } from "aphrodite";
+
+const MangaPageTest = () => {
+    const styles = StyleSheet.create({
+        reader: {},
+    });
+
+    return (
+        <div className={css(styles.reader)}>
+            <MangaPage
+                fit="comfortable"
+                rounded
+                radius={2}
+                url="https://cdn.discordapp.com/attachments/760399994791657472/1014393229279514694/Kininatteru_Hito_ga_Otoko_Jyanakatta_-_Ch.18.1_-_The_Two_of_Them_Shopping_on_a_Day_Off_Five_Years_From_Now_Hopefully_-_1.png" // This is why we use a Blob.
+            />
+            <Lightbar pages={24} />
+        </div>
+    );
+};
+
+export default MangaPageTest;
