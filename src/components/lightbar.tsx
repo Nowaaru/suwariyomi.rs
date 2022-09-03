1c5a061a2fcd87288d9434c28839f6b567c9f273
Nowaaru <zackyboy135@gmail.com>
2022-09-03 03:52:21 -0700

    [frontend] finalize lightbar

--- src/components/lightbar.tsx
+++ src/components/lightbar.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { StyleSheet, css } from "util/aphrodite";
 
 type LightbarPropsVertical = {
@@ -14,24 +14,46 @@ type LightbarPropsHorizontal = {
 };
 
 type LightbarTabProps = {
-    pageNumber: number,
-    selected?: boolean,
-    onClick?: () => void,
+    pageNumber: number;
+    selected?: boolean;
+    onClick?: (
+        e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        tab: number
+    ) => void;
 
     barPosition?: "left" | "right" | "top" | "bottom";
 };
 
 type LightbarProps = (LightbarPropsVertical | LightbarPropsHorizontal) & {
-    pages: number,
-    current?: number,
-    reversed?: boolean,
+    pages: number;
+    current?: number;
+    reversed?: boolean;
+    onTabClick?: LightbarTabProps['onClick'];
 };
 
 const LightbarTab = (props: LightbarTabProps) => {
-    const { barPosition = "bottom", selected } = props;
-    const barSizeProfile = (barPosition === "left" || barPosition === "right") ? "height" : "width";
+    const { barPosition = "bottom", pageNumber, onClick, selected } = props;
+    const barSizeProfile =
+        barPosition === "left" || barPosition === "right" ? "height" : "width";
     const oppBarSizeProfile = barSizeProfile === "height" ? "width" : "height";
 
+    const selectedColor = "#fb8e84";
+    const oppPosition = {
+        bottom: "top",
+        top: "bottom",
+        left: "right",
+        right: "left",
+    }[barPosition];
+
+    const selectedStyle = {
+        content: "' '",
+        position: "absolute",
+        [barSizeProfile]: "100%",
+        [oppBarSizeProfile]: "70%",
+        [barPosition]: "-25%",
+        transition: "background 0.15s ease-in",
+        background: `linear-gradient(to ${oppPosition}, ${selectedColor}66, ${selectedColor}00)`,
+    };
     const styles = useMemo(
         () =>
             StyleSheet.create({
@@ -40,7 +62,13 @@ const LightbarTab = (props: LightbarTabProps) => {
                     position: "relative", // to position the ::after properly
                     flexGrow: 1,
 
-                    [barPosition]: selected ? "25%" : "unset",
+                    [barPosition]: selected ? "5%" : "0",
+                    transition: `${barPosition} 0.15s ease-in`,
+                    ":hover": {
+                        "::before": selected ? {} : selectedStyle,
+                        [barPosition]: "5%",
+                        cursor: "pointer",
+                    },
                     "::after": {
                         content: "' '",
                         position: "absolute",
@@ -49,63 +77,138 @@ const LightbarTab = (props: LightbarTabProps) => {
                         [oppBarSizeProfile]: "4px",
                         backgroundColor: "#fb8e84",
                         boxSizing: "border-box",
-                    }
+                        boxShadow: "0px 0px #000000",
+                        borderRadius: "4px",
+                    },
+                },
+                gradientSelected: {
+                    "::before": selectedStyle,
+                },
+                notSelected: {
+                    opacity: 0,
+                },
+                lightbarNum: {
+                    opacity: 1,
+                    position: "absolute",
+                    display: "flex",
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    zIndex: "32",
+                    color: "white",
+                    fontWeight: "100",
+                    fontSize: "0.8rem",
+                    fontFamily: "Cascadia Code",
+                    transition: "opacity 0.15s ease-in-out",
+                    userSelect: "none",
+                    [oppPosition]: "8px",
+                    ":hover": {
+                        opacity: 1,
+                    },
                 },
             }),
         [props]
     );
 
-    return <div className={css(styles.lightbarTab)} />;
+    return (
+        <div
+            onClick={(e) => {
+                if (onClick) onClick(e, pageNumber);
+            }}
+            className={css(styles.lightbarTab)}
+        >
+            <span
+                className={css(
+                    styles.lightbarNum,
+                    selected ? styles.gradientSelected : false,
+                    !selected && styles.notSelected
+                )}
+            >
+                {pageNumber}
+            </span>
+        </div>
+    );
 };
 
 const Lightbar = (props: LightbarProps) => {
-    const { vertical: isVertical, right: isRight, top: isTop, current = 1, pages } = props;
+    const {
+        vertical: isVertical,
+        right: isRight,
+        top: isTop,
+        onTabClick,
+        current = 1,
+        pages,
+    } = props;
     if (pages <= 0) throw new Error("parameter pages is not zero-indexed");
 
+    const lightbarColor = "#FFFFFF";
     const positioningParameter = useMemo(() => {
         let finalResult: string;
         if (isVertical) {
-            if (isRight)
-                return finalResult = "right"
+            if (isRight) return (finalResult = "right");
 
-            return finalResult = "left";
+            return (finalResult = "left");
         }
 
-        if (isTop)
-            return finalResult = "top";
+        if (isTop) return (finalResult = "top");
 
-        return finalResult = "bottom";
-    }, [props])
+        return (finalResult = "bottom");
+    }, [props]);
+    const oppPosition = {
+        bottom: "top",
+        top: "bottom",
+        left: "right",
+        right: "left",
+    }[positioningParameter];
 
     const styles = useMemo(
         () =>
             StyleSheet.create({
                 lightBar: {
                     display: "flex",
+                    zIndex: "25",
                     position: "absolute",
                     justifyContent: "center",
+                    top: isVertical ? "0px" : "unset",
+                    flexDirection: isVertical ? "column" : "row",
                     [positioningParameter]: "0",
                     [isVertical ? "width" : "height"]: "8%",
                     [isVertical ? "height" : "width"]: "100%",
+
+                    "::after": {
+                        content: "' '",
+                        position: "absolute",
+                        [positioningParameter]: "0",
+                        [isVertical ? "width" : "height"]: "60%",
+                        [isVertical ? "height" : "width"]: "100%",
+
+                        zIndex: -1,
+                        background: `linear-gradient(to ${oppPosition}, ${lightbarColor}22, ${lightbarColor}00)`,
+                    },
                 },
             }),
-        []
+        [isVertical, oppPosition, lightbarColor, positioningParameter]
     );
 
     const generateTabs = useCallback(() => {
         const lightbarTabs: Array<JSX.Element> = [];
-        for (let i = 1; i < pages; i++ ) { // I expect pages to not be zero-indexed for this case
-            lightbarTabs.push(<LightbarTab pageNumber={i} selected={i === current} onClick={() => console.log(`Page ${i} selected.`)} />)
+        for (let i = 1; i <= pages; i++) {
+            // I expect pages to not be zero-indexed for this case
+            lightbarTabs.push(
+                <LightbarTab
+                    barPosition={positioningParameter}
+                    pageNumber={i}
+                    selected={i === current}
+                    onClick={onTabClick}
+                />
+            );
         }
 
         return lightbarTabs;
-    }, [LightbarTab, props])
+    }, [LightbarTab, props]);
 
-    return (
-        <div className={css(styles.lightBar)}>
-            {generateTabs()}
-        </div>
-    );
+    return <div className={css(styles.lightBar)}>{generateTabs()}</div>;
 };
 
 export default Lightbar;
