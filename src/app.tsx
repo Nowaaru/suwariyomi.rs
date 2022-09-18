2c5712f120e2bd129d976919b048dfcd43fd935e
Nowaaru <zackyboy135@gmail.com>
2022-09-18 13:13:08 -0700

    [frontend] clarity

--- src/app.tsx
+++ src/app.tsx
@@ -3,23 +3,18 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Page Imports
-import BaseClass from "pages/main";
 import Library from "pages/library";
 import View from "pages/view";
 import SplashScreen from "pages/splash";
 import Reader from "pages/reader";
 
-// Utiltiy
+
+
+// Utility
 import DefaultThemeDark from "assets/themes/dark";
 // TODO: Instead of directly writing the routes with the target page
 //      we should use a function to setup any additional logic (i.e. redirecting),
 //      and then return the logic after all is done.
-
-// TODO: Don't even think about using Enmaps. Use a Rust database library instead.
-
-// TODO: Instead of having settings handled by react, create a Settings module
-//      in Rust with a serializer function, then use Tauri to stream the serialized
-//      settings to the client.
 
 export default function App() {
     window.addEventListener("keydown", (e) => {
@@ -33,7 +28,7 @@ export default function App() {
             <ChakraProvider theme={extendTheme(DefaultThemeDark)}>
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/" element={<Reader />} />
+                        <Route path="/" element={<Library />} />
                         <Route path="reader" element={<Reader />} />
                         <Route path="view" element={<View />} />
                         <Route path="splash" element={<SplashScreen />} />
