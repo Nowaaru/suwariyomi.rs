8dce7e22fd6fa17f815945b5c165f5ae513a42a5
Nowaaru <zackyboy135@gmail.com>
2022-09-27 04:59:47 -0700

    [frontend] implement ipc interface

---
+++ src/util/ipc.ts
@@ -0,0 +1,74 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { Chapter, Manga } from "types/manga";
+
+const ipcFunctions = {
+    path: {
+        exists: async (path: string): Promise<boolean> => {
+            return invoke("path_exists", { path });
+        },
+    },
+    manga: {
+        getAll: async (source?: string): Promise<Array<Manga>> => {
+            return invoke("get_all_manga", { source });
+        },
+        getMultiple: async (
+            source: string,
+            ids: Array<string>
+        ): Promise<Array<Manga>> => {
+            return invoke("get_mangas", { source, ids });
+        },
+        get: async (id: string, source: string): Promise<Manga | undefined> => {
+            return invoke("get_manga", { id, source });
+        },
+
+        insert: async (manga: Manga): Promise<never> => {
+            return invoke("insert_manga", { manga });
+        },
+        remove: async (id: string, source: string): Promise<never> => {
+            return invoke("remove_manga", { id, source });
+        },
+        clear: async (): Promise<never> => {
+            return invoke("clear_manga", {});
+        },
+    },
+    chapters: {
+        getAll: async (
+            source?: string,
+            id?: string,
+            manga_id?: string
+        ): Promise<Array<Chapter>> => {
+            return invoke("get_all_chapters", { source, id, manga_id });
+        },
+        getMultiple: async (
+            source: string,
+            manga_id: string,
+            ids: Array<string>
+        ): Promise<Array<Chapter>> => {
+            return invoke("get_chapters", { source, manga_id, ids });
+        },
+        get: async (
+            source: string,
+            manga_id: string,
+            id: string
+        ): Promise<Array<Chapter>> => {
+            return invoke("get_chapter", { source, id, manga_id });
+        },
+
+        insert: async (chapter: Chapter): Promise<never> => {
+            return invoke("insert_chapter", { chapter });
+        },
+        remove: async (manga_id: string, id: string): Promise<never> => {
+            return invoke("remove_chapter", { manga_id, id });
+        },
+        clear: async (): Promise<never> => {
+            return invoke("clear_chapters", {});
+        },
+    },
+    sources: {
+        get: async (): Promise<Array<string>> => {
+            return invoke("get_sources", {});
+        },
+    },
+};
+
+export default ipcFunctions;
