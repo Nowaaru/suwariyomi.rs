8589d237245f229c422462e56d34b251a57c3d9f
Nowaaru <zackyboy135@gmail.com>
2022-09-28 09:35:19 -0700

    [frontend] add "to" field to Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,21 +1,25 @@
 import { Button, ButtonProps } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-const Back = (props: ButtonProps) => {
-    return (
-        <Link to="/library">
-            <Button
-                borderRadius={2}
-                backgroundColor={"#fb8e84"}
-                color="whitesmoke"
-                _hover={{
-                    bg: "#f88379",
-                }}
-                {...props}
-            >
-                Back
-            </Button>
-        </Link>
+const Back = (props: ButtonProps & { to?: string | null }) => {
+    const coreButton = (
+        <Button
+            borderRadius={2}
+            backgroundColor={"#fb8e84"}
+            color="whitesmoke"
+            _hover={{
+                bg: "#f88379",
+            }}
+            {...props}
+        >
+            {props.children ?? "Back"}
+        </Button>
+    );
+
+    return props.to !== null ? (
+        <Link to={props.to ?? "/library"}>{coreButton}</Link>
+    ) : (
+        coreButton
     );
 };
 
