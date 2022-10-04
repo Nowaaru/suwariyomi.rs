6f878eea6eafe52773e916b82ea500bcd530b2b9
Nowaaru <zackyboy135@gmail.com>
2022-10-04 01:59:17 -0700

    [frontend] implement custom style for select

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,11 +1,24 @@
-import ReactSelect, { GroupBase, Props } from "react-select";
+import ReactSelect, { GroupBase, Props, ActionMeta } from "react-select";
 
 function Select<
     Option,
     IsMulti extends boolean = false,
     Group extends GroupBase<Option> = GroupBase<Option>
 >(props: Props<Option, IsMulti, Group>): JSX.Element {
-    return <ReactSelect {...props} />;
+    return (
+        <ReactSelect
+            styles={{
+                option: (provided, state) => ({
+                    ...provided,
+                    borderBottom: "1px dotted pink",
+                    backgroundColor: "unset",
+                    fontWeight: state.isSelected ? "bolder" : "initial",
+                    color: state.isSelected ? "#fb8e84" : "#00000099",
+                }),
+            }}
+            {...props}
+        />
+    );
 }
 
 export default Select;
