5a3dc894d5738ddfa7846c8569e93b1807a7fb43
Nowaaru <zackyboy135@gmail.com>
2022-10-09 08:14:04 -0700

    feat(frontend/reader): finalize design of reader toolbar

--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,11 +1,12 @@
-import { SearchIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon, SearchIcon } from "@chakra-ui/icons";
 import {
-    Box,
     Button,
     ButtonGroup,
     Container,
-    Flex,
     IconButton,
+    Tooltip,
+    IconButtonProps,
+    Divider,
 } from "@chakra-ui/react";
 import { fetch, ResponseType } from "@tauri-apps/api/http";
 import { css, StyleSheet } from "aphrodite";
@@ -14,6 +15,17 @@ import Lightbar from "components/lightbar";
 import MangaPage from "components/mangapage";
 import _ from "lodash";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import {
+    MdCropFree,
+    MdFormatListNumbered,
+    MdHouse,
+    MdList,
+    MdLooksOne,
+    MdPublic,
+    MdSettings,
+    MdShare,
+    MdWeb,
+} from "react-icons/md";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import SourceHandler, { Source } from "util/sources";
 
@@ -34,11 +46,48 @@ type MangaData = {
     chapterId: string | null;
 };
 
+const IconButtonWithLabel = (
+    props: {
+        label: string;
+        color?: string;
+    } & Partial<IconButtonProps>
+) => {
+    const newProps: {
+        [K in keyof typeof props]?: IconButtonProps[keyof IconButtonProps];
+    } & { ["aria-label"]: string } = {
+        ["aria-label"]: props.label,
+    };
+
+    (Object.keys(props) as Array<keyof typeof props>).forEach(
+        (k) => (newProps[k] = props[k])
+    );
+
+    return (
+        <Tooltip label={props.label}>
+            <IconButton
+                _hover={{
+                    backgroundColor: props.color ?? "#f88379",
+                }}
+                variant="ghost"
+                {...newProps}
+            />
+        </Tooltip>
+    );
+};
+
 const Reader = () => {
     const futurePagesToLoad = 4;
     const Navigate = useNavigate();
     const [pageSources, setPageSources] = useState<Array<string>>([]);
     const pages = useRef<Array<Page>>([]);
+
+    const styles = StyleSheet.create({
+        reader: {
+            backgroundColor: "#0D1620",
+            width: "100vw",
+            height: "100vh",
+        },
+    });
 
     const [queryParams] = useSearchParams();
     const [sourceHandler, setSourceHandler] = useState<Source | null>(null);
@@ -260,37 +309,86 @@ const Reader = () => {
         );
     }, [currentPageNumber, downloadPage, currentPage]);
 
-    const styles = StyleSheet.create({
-        reader: {
-            backgroundColor: "#0D1620",
-            width: "100vw",
-            height: "100vh",
-        },
+    const lastMovedFadeThreshold = 1000;
+    const [forceShow, setForceShow] = useState(false);
+    const [mouseData, setMouseData] = useState({
+        x: 0,
+        y: 0,
+
+        lastMoved: 0,
     });
 
+    useEffect(() => {
+        const newInterval = setInterval(
+            () =>
+                setMouseData((oldData) => ({
+                    ...oldData,
+                    lastMoved: oldData.lastMoved + 100,
+                })),
+            100
+        );
+        return () => clearInterval(newInterval);
+    }, [setMouseData]);
+
+    const shouldHide =
+        lastMovedFadeThreshold < mouseData.lastMoved && !forceShow;
+
     return (
-        <div className={css(styles.reader)}>
+        <div
+            className={css(styles.reader)}
+            onMouseMove={({ clientX: x, clientY: y }) =>
+                setMouseData({ x, y, lastMoved: 0 })
+            }
+        >
             {currentMangaPage}
             <Container
                 position="absolute"
                 maxWidth="100%"
                 height="100px"
-                bottom="100px"
+                bottom="50px"
                 centerContent
             >
                 <Container
-                    backgroundColor="#000000CC"
+                    backgroundColor={forceShow ? "#0D1620" : "#0D1620CC"}
                     borderColor="#00000044"
                     borderWidth="2px"
                     borderRadius="4px"
                     padding="8px"
                     color="white"
-                    width="450px"
+                    maxWidth="450px"
+                    width="fit-content"
                     height="fit-content"
                     maxHeight="100%"
+                    display={shouldHide ? "none" : "initial"}
+                    onMouseEnter={() => setForceShow(true)}
+                    onMouseLeave={() => setForceShow(false)}
                 >
-                    <ButtonGroup gap="4" isAttached>
-                        <IconButton aria-label="Search" icon={<SearchIcon />} />
+                    <ButtonGroup height="100%">
+                        <IconButtonWithLabel
+                            icon={<MdPublic />}
+                            label="Open in Browser"
+                        />
+                        <IconButtonWithLabel
+                            icon={<MdLooksOne />}
+                            label="Single Page"
+                        />
+                        <IconButtonWithLabel icon={<MdShare />} label="Share" />
+                        <IconButtonWithLabel
+                            label="Open Chapter Index"
+                            onClick={() => Navigate(-1)}
+                            icon={<MdFormatListNumbered />}
+                        />
+                        <Divider orientation="vertical" />
+                        <IconButtonWithLabel
+                            label="Go Back"
+                            onClick={() => Navigate(-1)}
+                            icon={<MdHouse />}
+                        />
+                        <IconButtonWithLabel
+                            label="Open Settings"
+                            onClick={() => Navigate(-1)}
+                            icon={<MdSettings />}
+                        />
                     </ButtonGroup>
                 </Container>
             </Container>
