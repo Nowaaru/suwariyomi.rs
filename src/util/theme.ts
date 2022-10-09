ac1193e3328bca31d1eabddecfd0247767582519
Nowaaru <zackyboy135@gmail.com>
2022-10-09 04:41:23 -0700

    feat(frontend): implement theme handler

---
+++ src/util/theme.ts
@@ -0,0 +1,46 @@
+import chroma, { Color as CColor } from "chroma-js";
+import _ from "lodash";
+
+export type Color = CColor | string | number;
+export type ThemeRequired = {
+    background: Color;
+    accent: Color;
+} & { [k in string]: Color };
+
+const ThemeDefault = {
+    background: "#0D1620",
+    accent: "#fb8e84",
+};
+
+const convertToColors = (colors: ThemeRequired) => {
+    Object.values(colors).map((v: Color) => chroma(v));
+    return colors;
+};
+
+class ThemeHandler {
+    constructor(colors: ThemeRequired = ThemeDefault) {
+        this._colors = convertToColors(_.cloneDeep(colors));
+    }
+
+    public color(colorId: keyof typeof this.colors) {
+        if (this._colors[colorId])
+            return _.cloneDeep(this._colors[colorId]);
+    }
+
+    public colors() {
+        return _.cloneDeep(this._colors);
+    }
+
+    public apply(newColors: ThemeRequired) {
+        this._colors = {
+            ...ThemeDefault,
+            ...convertToColors(newColors),
+        };
+
+        return this;
+    }
+
+    private _colors: ThemeRequired;
+}
+
+export default new ThemeHandler();
