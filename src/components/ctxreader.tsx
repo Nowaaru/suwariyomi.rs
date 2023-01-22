2d01ac3d968fe2d9677a8489111e89974d3ecbaa
Nowaaru <zackyboy135@gmail.com>
2023-01-22 15:00:14 -0700

    feat(frontend/ctxreader): add icons

--- src/components/ctxreader.tsx
+++ src/components/ctxreader.tsx
@@ -10,6 +10,23 @@ import {
 } from "@chakra-ui/react";
 import { css, StyleSheet } from "aphrodite";
 import { useMemo } from "react";
+import { IconType } from "react-icons/lib";
+import {
+    MdDownload,
+    MdFirstPage,
+    MdFolderOpen,
+    MdHomeFilled,
+    MdLastPage,
+    MdLibraryBooks,
+    MdRemoveRedEye,
+    MdSave,
+    MdSettings,
+    MdShare,
+    MdSkipNext,
+    MdSkipPrevious,
+    MdViewComfy,
+} from "react-icons/md";
+import { Icon } from "@chakra-ui/react";
 
 enum ContextMenuItem {
     Save = "Save",
@@ -29,6 +46,7 @@ type ContextMenuProps = {
     onItemClick?: (
         itemClicked: ContextMenuItem | string
     ) => unknown | undefined;
+    overflowRef: React.RefObject<HTMLElement | JSX.Element>;
     onClose?: () => unknown | undefined;
     onOpen?: () => unknown | undefined;
     isOpen: boolean;
@@ -38,15 +56,26 @@ type ContextMenuProps = {
 };
 
 const convertStringToElement = (
-    item: ContextMenuItem | string,
+    _item: ContextMenuItem | readonly [ContextMenuItem, IconType] | string,
     onClick?: ContextMenuProps["onItemClick"]
-) => (
-    <MenuItem key={item} onClick={() => onClick?.(item)}>
-        {item}
-    </MenuItem>
-);
+) => {
+    const item = Array.isArray(_item) ? _item[0] : _item;
+    const icon = Array.isArray(_item) ? (_item[1] as IconType) : undefined;
+
+    return (
+        <MenuItem
+            backgroundColor="0D1620"
+            icon={icon ? <Icon as={icon} /> : undefined}
+            key={item}
+            onClick={() => onClick?.(item)}
+        >
+            {item}
+        </MenuItem>
+    );
+};
 
 const ContextMenu = ({
+    overflowRef,
     onItemClick,
     isOpen,
     onClose,
@@ -55,53 +84,80 @@ const ContextMenu = ({
     x,
     y,
 }: ContextMenuProps) => {
+    if (!overflowRef.current) return null;
+
     return (
-        <Box position="absolute" left={x} top={y}>
-            <Menu
-                computePositionOnMount
-                closeOnSelect
-                closeOnBlur
-                autoSelect
-                matchWidth
-                strategy="absolute"
-                isOpen={isOpen}
-                onClose={onClose}
-            >
-                <MenuList>
-                    <MenuGroup title="File">
-                        {[
-                            ContextMenuItem.Save,
-                            ContextMenuItem.SaveTo,
-                            ContextMenuItem.Share,
-                        ].map((item) =>
+        <Menu
+            closeOnSelect
+            preventOverflow
+            boundary={overflowRef.current as HTMLElement}
+            closeOnBlur
+            autoSelect
+            matchWidth
+            strategy="fixed"
+            isOpen={isOpen}
+            colorScheme="#0D1620"
+            onClose={onClose}
+        >
+            <Portal>
+                <MenuList
+                    left={x}
+                    top={y}
+                    position={"absolute"}
+                    backgroundColor="0D1620"
+                >
+                    <MenuGroup title="File" backgroundColor="0D1620">
+                        {
+                            // TODO: make this not shitty
+                            (
+                                [
+                                    [ContextMenuItem.Save, MdDownload],
+                                    [ContextMenuItem.SaveTo, MdFolderOpen],
+                                    [ContextMenuItem.Share, MdShare],
+                                ] as
+                                    | readonly [ContextMenuItem, IconType][]
+                                    | readonly ContextMenuItem[]
+                            ).map((item) =>
+                                convertStringToElement(item, onItemClick)
+                            )
+                        }
+                    </MenuGroup>
+                    <MenuDivider />
+                    <MenuGroup backgroundColor="0D1620" title="Navigation">
+                        {(
+                            [
+                                [ContextMenuItem.Chapters, MdLibraryBooks],
+                                [ContextMenuItem.ChapterNext, MdSkipNext],
+                                [
+                                    ContextMenuItem.ChapterPrevious,
+                                    MdSkipPrevious,
+                                ],
+                                [ContextMenuItem.SkipToBeginning, MdFirstPage],
+                                [ContextMenuItem.SkipToEnd, MdLastPage],
+                            ] as
+                                | readonly [ContextMenuItem, IconType][]
+                                | readonly ContextMenuItem[]
+                        ).map((item) =>
                             convertStringToElement(item, onItemClick)
                         )}
                     </MenuGroup>
                     <MenuDivider />
-                    <MenuGroup title="Navigation">
-                        {[
-                            ContextMenuItem.Chapters,
-                            ContextMenuItem.ChapterNext,
-                            ContextMenuItem.ChapterPrevious,
-                            ContextMenuItem.SkipToBeginning,
-                            ContextMenuItem.SkipToEnd,
-                        ].map((item) =>
-                            convertStringToElement(item, onItemClick)
-                        )}
-                    </MenuGroup>
-                    <MenuDivider />
-                    <MenuGroup title="Miscellaneous">
-                        {[
-                            ContextMenuItem.Settings,
-                            ContextMenuItem.ToView,
-                            ContextMenuItem.ToHome,
-                        ].map((item) =>
+                    <MenuGroup backgroundColor="0D1620" title="Miscellaneous">
+                        {(
+                            [
+                                [ContextMenuItem.Settings, MdSettings],
+                                [ContextMenuItem.ToView, MdViewComfy],
+                                [ContextMenuItem.ToHome, MdHomeFilled],
+                            ] as
+                                | readonly [ContextMenuItem, IconType][]
+                                | readonly ContextMenuItem[]
+                        ).map((item) =>
                             convertStringToElement(item, onItemClick)
                         )}
                     </MenuGroup>
                 </MenuList>
-            </Menu>
-        </Box>
+            </Portal>
+        </Menu>
     );
 };
 
